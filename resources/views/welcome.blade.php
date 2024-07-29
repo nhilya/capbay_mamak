b20a486bc9720283e00966b7cb309305c06b4071
nhilya <nuhailya.dev@gmail.com>
2024-07-29 22:12:33 +0800

    Modify the styling of the welcome blade.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,19 +7,23 @@
     <style>
         body {
             display: flex;
+            flex-direction: column;
             justify-content: center;
             align-items: center;
             height: 100vh;
             margin: 0;
+            font-family: "Inter", sans-serif;
+            background-color: F3F3F6;
         }
         .auth-links {
             display: flex;
             flex-direction: column;
-            justify-content: center;
+            gap: 1rem;
             align-items: center;
-        }
-        .auth-links a {
-            margin-bottom: 1rem; /* Adjust the spacing as needed */
+            background-color: #F5F5F5;
+            padding: 2rem;
+            border-radius: 0.5rem;
+            width: 300px;
         }
     </style>
 </head>
@@ -27,23 +31,15 @@
     @if (Route::has('login'))
         <nav class="auth-links">
             @auth
-                <a
-                    href="{{ url('/dashboard') }}"
-                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                >
+                <a href="{{ url('/dashboard') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ms-3">
                     Dashboard
                 </a>
             @else
-                <a href="{{ route('login') }}"
-                    class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                >
+                <a href="{{ route('login') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ms-3">
                     Log in
                 </a>
-                <br>
                 @if (Route::has('register'))
-                    <a href="{{ route('register') }}"
-                        class="rounded-md px-3 py-2 text-black ring-1 ring-transparent transition hover:text-black/70 focus:outline-none focus-visible:ring-[#FF2D20] dark:text-white dark:hover:text-white/80 dark:focus-visible:ring-white"
-                    >
+                    <a href="{{ route('register') }}" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150 ms-3">
                         Register
                     </a>
                 @endif
